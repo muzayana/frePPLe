8bdf3c220b3a79ef3bf6da311492052ea96ddd75
Johan De Taeye <jdetaeye@frepple.com>
2014-02-24 10:10:41 +0100

    better handling of "failed coordination"

--- src/solver/solverdemand.cpp
+++ src/solver/solverdemand.cpp
@@ -259,7 +259,7 @@ DECLARE_EXPORT void SolverMRP::solve(const Demand* l, void* v)
           if (loglevel>=2)
             logger << "Demand '" << l << "' plans coordination." << endl;
           data->getSolver()->setLogLevel(0);
-          double tmpresult = data->state->a_qty;
+          double tmpresult = 0;
           try
           {
             for(double remainder = data->state->a_qty;
@@ -275,6 +275,7 @@ DECLARE_EXPORT void SolverMRP::solve(const Demand* l, void* v)
                 logger << "Warning: Demand '" << l << "': Failing coordination" << endl;
                 break;
               }
+              tmpresult += data->state->a_qty;
             }
           }
           catch (...)
