a9f33d40eec6f393cd71500c3e93972630ecbf1d
Johan De Taeye <jdetaeye@frepple.com>
2015-02-13 02:06:36 +0100

    Adding ability to drill down into order details in the forecast report

--- contrib/django/freppledb/forecast/views.py
+++ contrib/django/freppledb/forecast/views.py
@@ -12,6 +12,7 @@ from datetime import datetime
 from decimal import Decimal
 import json
 
+from django.contrib.admin.util import unquote
 from django.db import connections, transaction
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import capfirst
@@ -21,9 +22,10 @@ from django.http import HttpResponse, HttpResponseForbidden, Http404
 from freppledb.forecast.models import Forecast, ForecastDemand, ForecastPlan
 from freppledb.common.db import python_date
 from freppledb.common.report import GridPivot, GridFieldText, GridFieldInteger, GridFieldDate
-from freppledb.common.report import GridReport, GridFieldBool, GridFieldLastModified
-from freppledb.common.report import GridFieldChoice, GridFieldNumber
+from freppledb.common.report import GridReport, GridFieldBool, GridFieldLastModified, GridFieldCurrency
+from freppledb.common.report import GridFieldChoice, GridFieldNumber, GridFieldDateTime, GridFieldDuration
 from freppledb.input.views import PathReport
+from freppledb.input.models import Demand
 
 
 class ForecastList(GridReport):
@@ -289,3 +291,42 @@ class UpstreamForecastPath(PathReport):
         raise Http404("No supply path defined for forecast %s" % entity)
     except ObjectDoesNotExist:
       raise Http404("forecast %s doesn't exist" % entity)
+
+
+class OrderReport(GridReport):
+  '''
+  A list report to show demands.
+  '''
+  template = 'input/demandlist.html'
+  title = _("Demand List")
+  model = Demand
+  frozenColumns = 1
+
+  @ classmethod
+  def basequeryset(reportclass, request, args, kwargs):
+    fcst = Forecast.objects.using(request.database).get(name__exact=unquote(request.GET['forecast']))
+    return Demand.objects.filter(
+      item__lft__gte=fcst.item.lft,
+      item__lft__lt=fcst.item.rght,
+      customer__lft__gte=fcst.customer.lft,
+      customer__lft__lt=fcst.customer.rght
+      )
+
+  rows = (
+    GridFieldText('name', title=_('name'), key=True, formatter='demand'),
+    GridFieldText('item', title=_('item'), field_name='item__name', formatter='item'),
+    GridFieldText('customer', title=_('customer'), field_name='customer__name', formatter='customer'),
+    GridFieldText('description', title=_('description')),
+    GridFieldText('category', title=_('category')),
+    GridFieldText('subcategory', title=_('subcategory')),
+    GridFieldDateTime('due', title=_('due')),
+    GridFieldNumber('quantity', title=_('quantity')),
+    GridFieldText('operation', title=_('delivery operation'), formatter='operation'),
+    GridFieldInteger('priority', title=_('priority')),
+    GridFieldText('owner', title=_('owner'), formatter='demand'),
+    GridFieldChoice('status', title=_('status'), choices=Demand.demandstatus),
+    GridFieldDuration('maxlateness', title=_('maximum lateness')),
+    GridFieldNumber('minshipment', title=_('minimum shipment')),
+    GridFieldText('source', title=_('source')),
+    GridFieldLastModified('lastmodified'),
+    )
