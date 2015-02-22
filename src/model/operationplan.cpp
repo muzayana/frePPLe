852d91ecf4e6e1266588b3f7d2e57b313f12cd44
Johan De Taeye <jdetaeye@frepple.com>
2015-02-22 10:34:50 +0100

    adding generic way to find an object in a category given its primary key

--- src/model/operationplan.cpp
+++ src/model/operationplan.cpp
@@ -26,7 +26,7 @@ int OperationPlan::initialize()
 {
   // Initialize the metadata
   OperationPlan::metacategory = new MetaCategory("operationplan", "operationplans",
-      OperationPlan::createOperationPlan, OperationPlan::writer);
+      OperationPlan::createOperationPlan, OperationPlan::writer, OperationPlan::finder);
   OperationPlan::metadata = new MetaClass("operationplan", "operationplan");
 
   // Initialize the Python type
@@ -805,6 +805,13 @@ DECLARE_EXPORT void OperationPlan::writer(const MetaCategory* c, Serializer* o)
       o->writeElement(*c->typetag, *i);
     o->EndList(*c->grouptag);
   }
+}
+
+
+DECLARE_EXPORT Object* OperationPlan::finder(const string& key)
+{
+  unsigned long id = strtoul(key.c_str(), NULL, 0);
+  return OperationPlan::findId(id);
 }
 
 
