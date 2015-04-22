80acc1a9c3c4409f3de4280e5ceefeee5a833488
Johan De Taeye <jdetaeye@frepple.com>
2015-04-22 10:35:01 +0200

    correction for grid rows initially hidden

--- contrib/django/freppledb/common/report.py
+++ contrib/django/freppledb/common/report.py
@@ -1490,10 +1490,10 @@ class GridPivot(GridReport):
     count = -1
     for (index, hidden, width) in rows:
       count += 1
-      result.append(u"{%s,width:%s,counter:%d,frozen:true%s%s,searchoptions:{searchhidden: true},fixed:true}" % (
+      result.append(u"{%s,width:%s,counter:%d,frozen:true%s,hidden:%s,searchoptions:{searchhidden: true},fixed:true}" % (
         cls.rows[index], width, index,
         is_popup and ',popup:true' or '',
-        hidden and not cls.rows[index].hidden and ',hidden:true' or ''
+        hidden and 'true' or 'false'
         ))
     if mode == "graph":
       result.append(
