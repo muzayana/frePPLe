40685e67aaaad0f5fa5fdb8a9e047488e32035f7
Johan De Taeye <jdetaeye@frepple.com>
2015-10-14 17:23:02 +0200

    draft openbravo upload view

---
+++ contrib/django/freppledb/openbravo/urls.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2015 by frePPLe bvba
+#
+# All information contained herein is, and remains the property of frePPLe.
+# You are allowed to use and modify the source code, as long as the software is used
+# within your company.
+# You are not allowed to distribute the software, either in the form of source code
+# or in the form of compiled binaries.
+#
+
+from django.conf.urls import patterns
+
+from freppledb.openbravo.views import Upload
+
+# Automatically add these URLs when the application is installed
+autodiscover = True
+
+urlpatterns = patterns(
+  '',  # Prefix
+  # Model list reports, which override standard admin screens
+  (r'^openbravo/upload/$', Upload),
+  )
---
+++ contrib/django/freppledb/openbravo/views.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2015 by frePPLe bvba
+#
+# All information contained herein is, and remains the property of frePPLe.
+# You are allowed to use and modify the source code, as long as the software is used
+# within your company.
+# You are not allowed to distribute the software, either in the form of source code
+# or in the form of compiled binaries.
+#
+import json
+
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse
+
+
+@login_required
+@csrf_protect
+def Upload(request):
+  data = json.loads(request.body.decode(request.encoding))
+  print(data)
+  return HttpResponse(content="OK")
