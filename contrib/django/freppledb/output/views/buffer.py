d0dc5bd1408e8b0e4cae7eeda59a708960303603
Johan De Taeye <jdetaeye@frepple.com>
2015-12-04 13:19:28 +0100

    inventory report refactoring to support custom attributes

--- contrib/django/freppledb/output/views/buffer.py
+++ contrib/django/freppledb/output/views/buffer.py
@@ -13,7 +13,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.text import capfirst
 from django.utils.encoding import force_text
 
-from freppledb.input.models import Buffer
+from freppledb.boot import getAttributeFields
+from freppledb.input.models import Buffer, Item, Location
 from freppledb.output.models import FlowPlan
 from freppledb.common.db import sql_max, sql_min, python_date, string_agg
 from freppledb.common.report import GridReport, GridPivot, GridFieldText, GridFieldNumber
@@ -42,6 +43,20 @@ class OverviewReport(GridPivot):
     )
 
   @classmethod
+  def initialize(reportclass, request):
+    if reportclass._attributes_added != 2:
+      reportclass._attributes_added = 2
+      reportclass.attr_sql = ''
+      # Adding custom item attributes
+      for f in getAttributeFields(Item, related_name_prefix="item"):
+        reportclass.rows += (f,)
+        reportclass.attr_sql += 'item.%s, ' % f.name.split('__')[-1]
+      # Adding custom location attributes
+      for f in getAttributeFields(Location, related_name_prefix="location"):
+        reportclass.rows += (f,)
+        reportclass.attr_sql += 'location.%s, ' % f.name.split('__')[-1]
+
+  @classmethod
   def extra_context(reportclass, request, *args, **kwargs):
     if args and args[0]:
       request.session['lasttab'] = 'plan'
@@ -52,8 +67,8 @@ class OverviewReport(GridPivot):
     else:
       return {}
 
-  @staticmethod
-  def query(request, basequery, sortsql='1 asc'):
+  @classmethod
+  def query(reportclass, request, basequery, sortsql='1 asc'):
     cursor = connections[request.database].cursor()
     basesql, baseparams = basequery.query.get_compiler(basequery.db).as_sql(with_col_aliases=False)
 
@@ -87,16 +102,22 @@ class OverviewReport(GridPivot):
 
     # Execute the actual query
     query = '''
-      select buf.name as row1, buf.item_id as row2, buf.location_id as row3,
-             d.bucket as col1, d.startdate as col2, d.enddate as col3,
-             coalesce(sum(%s),0.0) as consumed,
-             coalesce(-sum(%s),0.0) as produced
+      select
+        invplan.buffer_id, item.name, location.name, %s
+        invplan.bucket, invplan.startdate, invplan.enddate,
+        invplan.consumed, invplan.produced
+      from (
+        select
+          buf.name as buffer_id,
+          d.bucket as bucket, d.startdate as startdate, d.enddate as enddate,
+          coalesce(sum(%s),0.0) as consumed,
+          coalesce(-sum(%s),0.0) as produced
         from (%s) buf
         -- Multiply with buckets
         cross join (
              select name as bucket, startdate, enddate
              from common_bucketdetail
-             where bucket_id = '%s' and enddate > '%s' and startdate < '%s'
+             where bucket_id = %%s and enddate > %%s and startdate < %%s
              ) d
         -- Include child buffers
         inner join buffer
@@ -106,40 +127,57 @@ class OverviewReport(GridPivot):
         on buffer.name = out_flowplan.thebuffer
         and d.startdate <= out_flowplan.flowdate
         and d.enddate > out_flowplan.flowdate
-        and out_flowplan.flowdate >= '%s'
-        and out_flowplan.flowdate < '%s'
+        and out_flowplan.flowdate >= %%s
+        and out_flowplan.flowdate < %%s
         -- Grouping and sorting
         group by buf.name, buf.item_id, buf.location_id, buf.onhand, d.bucket, d.startdate, d.enddate
-        order by %s, d.startdate
+        ) invplan
+      left outer join buffer on
+        invplan.buffer_id = buffer.name
+      left outer join item on
+        buffer.item_id = item.name
+      left outer join location on
+        buffer.location_id = location.name
+      order by %s, invplan.startdate
       ''' % (
-        sql_max('out_flowplan.quantity', '0.0'), sql_min('out_flowplan.quantity', '0.0'),
-        basesql, request.report_bucket, request.report_startdate, request.report_enddate,
-        request.report_startdate, request.report_enddate, sortsql
+        reportclass.attr_sql, sql_max('out_flowplan.quantity', '0.0'), sql_min('out_flowplan.quantity', '0.0'),
+        basesql, sortsql
       )
-    cursor.execute(query, baseparams)
+    cursor.execute(query, baseparams + (request.report_bucket, request.report_startdate, request.report_enddate,
+        request.report_startdate, request.report_enddate))
 
     # Build the python result
     prevbuf = None
     for row in cursor.fetchall():
+      numfields = len(row)
       if row[0] != prevbuf:
         prevbuf = row[0]
         startoh = startohdict.get(prevbuf, 0)
-        endoh = startoh + float(row[6] - row[7])
+        endoh = startoh + float(row[numfields-2] - row[numfields-1])
       else:
         startoh = endoh
-        endoh += float(row[6] - row[7])
-      yield {
+        endoh += float(row[numfields-2] - row[numfields-1])
+      res =  {
         'buffer': row[0],
         'item': row[1],
         'location': row[2],
-        'bucket': row[3],
-        'startdate': python_date(row[4]),
-        'enddate': python_date(row[5]),
+        'bucket': row[numfields-5],
+        'startdate': python_date(row[numfields-4]),
+        'enddate': python_date(row[numfields-3]),
         'startoh': round(startoh, 1),
-        'produced': round(row[6], 1),
-        'consumed': round(row[7], 1),
+        'produced': round(row[numfields-2], 1),
+        'consumed': round(row[numfields-1], 1),
         'endoh': round(endoh, 1),
         }
+      # Add attribute fields
+      idx = 3
+      for f in getAttributeFields(Item, related_name_prefix="item"):
+        res[f.field_name] = row[idx]
+        idx += 1
+      for f in getAttributeFields(Location, related_name_prefix="location"):
+        res[f.field_name] = row[idx]
+        idx += 1
+      yield res
 
 
 class DetailReport(GridReport):
