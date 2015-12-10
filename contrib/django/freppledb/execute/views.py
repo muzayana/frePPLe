d044340c3fc8ece634357924acaf9dc41f7893c0
jranita <jranita@frepple.com>
2015-12-10 15:48:17 +0100

    String updates in Execute

--- contrib/django/freppledb/execute/views.py
+++ contrib/django/freppledb/execute/views.py
@@ -54,6 +54,7 @@ class TaskReport(GridReport):
 
   rows = (
     GridFieldInteger('id', title=_('identifier'), key=True),
+    #. Translators: Translation included with Django
     GridFieldText('name', title=_('name'), editable=False, align='center'),
     GridFieldDateTime('submitted', title=_('submitted'), editable=False, align='center'),
     GridFieldDateTime('started', title=_('started'), editable=False, align='center'),
@@ -61,6 +62,7 @@ class TaskReport(GridReport):
     GridFieldText('status', title=_('status'), editable=False, align='center', extra="formatter:status"),
     GridFieldText('message', title=_('message'), editable=False, width=500),
     GridFieldText('arguments', title=_('arguments'), editable=False),
+    #. Translators: Translation included with Django
     GridFieldText('user', title=_('user'), field_name='user__username', editable=False, align='center'),
     )
 
